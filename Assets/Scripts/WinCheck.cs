30fca1f8a5b4afb7a0fd3640e000d5b3efb46abb
david85258 <marcos14david7@gmail.com>
2025-05-22 17:11:38 +0200

    Win

--- Assets/Scripts/WinCheck.cs
+++ Assets/Scripts/WinCheck.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 
 public class WinCheck1 : MonoBehaviour
 {
-    
-    
+
+    public GameObject player;
     
     // Update is called once per frame
     void Update()
     {
-        
+        Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, new Vector2(0f, 0f), 0f);
+
+        foreach (var hit in hits)
+        {
+            if (hit.gameObject == player) // para no detectarse a sí mismo
+            {
+                Debug.Log("Win");
+            }
+        }
     }
 }
